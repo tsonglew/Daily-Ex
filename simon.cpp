5b908fa852c260f3c0d364b2cc92a101e2bbf5c2
kasheemlew <417879751@qq.com>
2015-12-12 02:02:13 +0800

    151212

---
+++ simon.cpp
@@ -0,0 +1,24 @@
+// define function simon
+#include <iostream>
+void simon(int);    // function prototype for simon()
+
+int main()
+{
+    using namespace std;
+
+    simon(3);    // call the simon() function
+    cout << "Pick a integer:";
+    int count;
+    cin >> count;
+    simon(count);    // call it again
+    cout << "Done!" << endl;
+    return 0;
+}
+
+void simon(int n)    // define the simon() function
+{
+    using namespace std;
+
+    cout << "Simon says touch your toes " << n << " times." << endl;
+            // void functions don't need return statements
+}
