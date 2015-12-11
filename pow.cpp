39a0dd537ed047ac7cf29a8f17cc2d15b092e7ef
kasheemlew <417879751@qq.com>
2015-12-12 01:42:49 +0800

    151212

---
+++ pow.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+
+int main(void)
+{
+    using namespace std;
+
+    double answer;
+    double pow(double, double);
+    answer = pow(5.0, 8.0);
+    cout << answer << endl;
+    return 0;
+}
