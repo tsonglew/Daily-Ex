149817c9d3d5b05c060d93b42a7ce3e3ae328bf2
kasheemlew <417879751@qq.com>
2016-02-06 10:49:07 +0800

    160206

---
+++ express.cpp
@@ -0,0 +1,18 @@
+// express.cpp -- values of expressions
+#include <iostream>
+int main()
+{
+    using namespace std;
+    int x;
+
+    cout << "The expression x = 100 has the value ";
+    cout << (x = 100) << endl;
+    cout << "The expression x < 3 has the value ";
+    cout << (x < 3) << endl;
+    cout.setf(ios_base::boolalpha);    // a newer C++ feature
+    cout << "The expression x < 3 has the value ";
+    cout << (x < 3) << endl;
+    cout << "The expression x > 3 has the value ";
+    cout << (x > 3) << endl;
+    return 0;
+}
