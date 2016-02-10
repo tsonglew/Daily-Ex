af472f12e48c6be91f5ea3ff338369180d58512d
kasheemlew <417879751@qq.com>
2016-02-10 18:19:48 +0800

    160210

---
+++ nested.cpp
@@ -0,0 +1,35 @@
+// nested.cpp -- nested loops and 2-D array
+#include <iostream>
+const int Cities = 5;
+const int Years = 4;
+int main()
+{
+    using namespace std;
+    const char * cities[Cities] =
+            {
+                    "Gribble City",
+                    "Gribbletown",
+                    "New Gribble",
+                    "San Gribble",
+                    "Gribble Vista"
+            };
+
+    int maxtemps[Years][Cities] =
+            {
+                    {96, 100, 87, 101, 105},
+                    {96, 98, 91, 107, 104},
+                    {97, 101, 93, 108, 107},
+                    {98, 103, 95, 109, 108}
+            };
+
+    cout << "Maximum temperatures for 2008 - 2011\n\n";
+    for (int city = 0; city < Cities; ++city)
+    {
+        cout << cities[city] << ":\t";
+        for (int year = 0; year < Years; ++year)
+            cout << maxtemps[year][city] << "\t";
+        cout << endl;
+    }
+
+    return 0;
+}
