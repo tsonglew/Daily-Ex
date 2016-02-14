6809a87ed376ef516f2118f0aabb07ca51f03bb8
kasheemlew <417879751@qq.com>
2016-02-14 14:58:22 +0800

    160214

---
+++ outfile.cpp
@@ -0,0 +1,42 @@
+// outfile.cpp -- writing to a file
+#include <iostream>
+#include <fstream>
+
+int main()
+{
+    using namespace std;
+
+    char automobile[50];
+    int year;
+    double a_price, d_price;
+
+    ofstream outFile;    // create object for output
+    outFile.open("carinfo.txt");
+
+    cout << "Enter the make and model of automobile: ";
+    cin.getline(automobile, 50);
+    cout << "Enter the model year: ";
+    cin >> year;
+    cout << "Enter the original asking price: ";
+    cin >> a_price;
+    d_price = 0.913 * a_price;
+
+    cout << fixed;
+    cout.precision(2);
+    cout.setf(ios_base::showpoint);
+    cout << "Make and model: " << automobile << endl;
+    cout << "Year: " << year << endl;
+    cout << "Was asking $" << a_price << endl;
+    cout << "Now asking $" << d_price << endl;
+
+    outFile << fixed;
+    outFile.precision(2);
+    outFile.setf(ios_base::showpoint);
+    outFile << "Make and model: " << automobile << endl;
+    outFile << "Year: " << year << endl;
+    outFile << "Was asking $" << a_price << endl;
+    outFile << "Now asking $" << d_price << endl;
+
+    outFile.close();
+    return 0;
+}
