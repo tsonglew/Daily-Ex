dd0cf9f07050d94579ee6788b4f58c27e3aa628f
kasheemlew <417879751@qq.com>
2016-02-19 16:50:08 +0800

    160219

---
+++ twoarg.cpp
@@ -0,0 +1,30 @@
+// twoarg.cpp -- a function with 2 arguments
+#include <iostream>
+using namespace std;
+void n_chars(char, int);
+int main()
+{
+    int times;
+    char ch;
+
+    cout << "Enter a character: ";
+    cin >> ch;
+    while (ch != 'q')
+    {
+        cout << "Enter an integer: ";
+        cin >> times;
+        n_chars(ch, times);
+        cout << "\nEnter another character or press the "
+             << "q-key to quit: ";
+        cin >> ch;
+    }
+    cout << "The value of time is " << times << ".\n";
+    cout << "Bye\n";
+    return 0;
+}
+
+void n_chars(char c, int n)
+{
+    while (n-- > 0)
+        cout << c;
+}
