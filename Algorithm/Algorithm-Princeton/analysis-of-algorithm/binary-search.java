9dc1bc9b7ad266a2193408c34abb2be827ee8868
kasheemlew <417879751@qq.com>
2016-10-06 21:41:36 +0800

    updated

---
+++ Algorithm/Algorithm-Princeton/analysis-of-algorithm/binary-search.java
@@ -0,0 +1,12 @@
+public static int binarySearch(int a[], int key)
+{
+    int lo = 0, hi = a.length - 1;
+    while (lo <= hi)
+    {
+        int mid = lo + (hi - lo)/2;
+        if (key < a[mid]) hi = mid - 1;
+        else if (key > a[mid]) lo = mid + 1;
+        else return mid;
+    }
+    return -1;
+}
