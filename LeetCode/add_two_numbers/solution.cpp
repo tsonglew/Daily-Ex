2264b7b1bb323c09120e7b4d195ca6aa97f43b1b
kasheemlew <417879751@qq.com>
2017-02-27 18:59:11 +0800

    updated add two nums

--- LeetCode/add_two_numbers/solution.cpp
+++ LeetCode/add_two_numbers/solution.cpp
@@ -9,17 +9,16 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode head(0);
-        ListNode *headptr = &head;
-        int flag = 0;
-        int sum;
-        while(l1||l2||flag){
-            sum = (l1?l1->val:0) + (l2?l2->val:0) + flag;
-            flag = sum / 10;
-            headptr->next = new ListNode(sum%10);
-            headptr = headptr->next;
-            l1 = l1? l1->next: l1;
-            l2 = l2? l2->next: l2;
+        int carry = 0;
+        ListNode *headptr = new ListNode(0);
+        ListNode *p = headptr;
+        while(l1||l2||carry){
+            int sum = (l1?l1->val:0) + (l2?l2->val:0) + carry;
+            carry =  sum / 10;
+            p->next = new ListNode(sum % 10);
+            p = p->next;
+            l1 = l1?l1->next:l1;
+            l2 = l2?l2->next:l2;
         }
         return headptr->next;
     }
