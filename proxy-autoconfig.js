3898e9ef231d82a7321322d1cfdc39beea689a1b
kasheemlew <417879751@qq.com>
2017-07-19 12:07:43 +0800

    add proxy-autoconfig

---
+++ proxy-autoconfig.js
@@ -0,0 +1,9 @@
+function FindProxyForURL(url, host) {
+    if (url.substring(0, 5) == "http:") {
+        return "PROXY http-proxy.mydomain.com:8080"
+    } else if (url.substring(0, 4) == "ftp:") {
+        return "PROXY ftp-proxy.mydomain.com:8080"
+    } else {
+        return "DIRECT"
+    }
+}
