7589b259070766374e1848b06cbdfc853f5cc20e
kasheemlew <417879751@qq.com>
2017-07-25 17:53:37 +0800

    updated

--- TensorFlowEx/tensorflow-DL-demo/coreTutorialTest.py
+++ TensorFlowEx/tensorflow-DL-demo/coreTutorialTest.py
@@ -1,38 +1,33 @@
+import numpy as np 
 import tensorflow as tf 
 
-# Constants are initialized when calling tf.constant
-node1 = tf.constant(3.0, dtype=tf.float32)
-node2 = tf.constant(4.0)    # also tf.float32 implicitly
-# print(node1, node2)
-
-sess = tf.Session()
-# print(sess.run([node1, node2]))
-
-node3 = tf.add(node1, node2)
-#print("node3:", node3)
-#print("sess.run(node3):", sess.run(node3))
-
-a = tf.placeholder(tf.float32)
-b = tf.placeholder(tf.float32)
-addr_node = a + b
-
-#print(sess.run(addr_node, {a: 3, b: 4}))
-#print(sess.run(addr_node, {a: [1, 3], b: [2, 4]}))
-add_and_triple = addr_node * 3
-#print(sess.run(add_and_triple, {a: 3, b: 4.5}))
-
+# Model Parameters
 W = tf.Variable([.3], dtype=tf.float32)
 b = tf.Variable([-.3], dtype=tf.float32)
+
+# Model input and output
 x = tf.placeholder(tf.float32)
 linear_model = W * x + b
-
-# Initialize variables
-init = tf.global_variables_initializer()
-sess.run(init)
-
-print(sess.run(linear_model, {x: [1, 2, 3, 4]}))
-
 y = tf.placeholder(tf.float32)
-squared_deltas = tf.square(y - linear_model)
-loss = tf.reduce_sum(squared_deltas)
-print(sess.run(loss, {x:[1,2,3,4], y:[0,-1,-2,-3]}))
+
+# loss
+loss = tf.reduce_sum(tf.square(linear_model - y))
+
+# Optimizer
+optimizer = tf.train.GradientDescentOptimizer(0.01)
+train = optimizer.minimize(loss)
+
+# Training data
+x_train = [1,2,3,4]
+y_train = [0,-1,-2,-3]
+
+# train loop
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+for _ in range(1000):
+    sess.run(train, {x:x_train, y:y_train})
+
+# Evaluate accuracy
+curr_W, curr_b, curr_loss = sess.run([W, b, loss] , {x:x_train, y:y_train})
+print("W: %s b: %s loss: %s"%(curr_W, curr_b, curr_loss))
